882afaa093c08ed0502d386f7cfb4078ed360d20
CarolineCarrot <63738354+CarolineCarrot@users.noreply.github.com>
2020-04-28 23:56:33 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,254 @@
-let mySprite = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . 5 . . . . . . . . . 5 . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . 5 
-. 5 5 . . . . . . . . . . . . 5 
-. . 5 . . . . . . . . . . . 5 5 
-. . 5 5 . . . . . . . . . 5 . . 
-. . . 5 5 . . . . . . . 5 5 . . 
-. . . . 5 5 5 5 5 5 5 5 5 . . . 
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, SpriteKind.Player)
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+1 d b b b d 1 1 1 b b b 1 1 d d 
+1 d 1 b b d d 1 d b b b b 1 1 d 
+d d 1 1 1 1 b b d 1 d d d 1 1 1 
+1 1 1 1 1 1 1 b 1 1 1 d d b 1 1 
+b d 1 1 1 1 1 d d 1 1 d d d b b 
+b d d d d 1 b b 1 d 1 1 b d 1 d 
+b b b b b d d d d d 1 1 1 b d d 
+1 1 1 1 1 b 1 b b 1 1 1 1 1 b d 
+b b 1 1 d 1 1 d b b 1 d d d 1 1 
+d b b b d 1 1 d d b b 1 1 1 1 1 
+d d b b d 1 1 d d d d d 1 1 b d 
+d d d 1 1 1 d d b b b b 1 b b d 
+d d d b b b b b b b 1 1 1 b d d 
+d d d d 1 d d 1 d 1 1 1 b b 1 1 
+d b b b d d b 1 b b 1 d d d d d 
+b b d d 1 1 b 1 d b b 1 b 1 1 d 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+. . . d d 1 d d 1 . . . . . . . 
+d d 1 1 1 1 d 1 1 1 1 1 1 d . . 
+. d d d 1 1 1 d d d 1 1 1 d . . 
+. . . d 1 1 1 1 1 1 d 1 1 1 d . 
+. . d 1 1 1 1 1 1 1 1 d 1 1 1 d 
+. d d d d d d d 1 1 d d d 1 1 1 
+d 1 1 d d . . . . . . . . d d 1 
+d d d . . . . . . . . . . . . . 
+. . . . . . d d d d . . . . . . 
+. . . . d 1 1 1 1 1 . . . . . . 
+. . d 1 1 d 1 1 d d 1 1 . . . . 
+d d d 1 1 1 d d 1 1 1 1 d d . . 
+d 1 1 1 1 d 1 1 1 1 1 1 1 1 1 . 
+d 1 1 1 d 1 1 d d 1 1 1 1 1 d . 
+. d d d d d . . . d d 1 d d . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+. . . . . d d d d d . . . . . . 
+. . . . d d 1 d d 1 1 d . . . . 
+. . . . d 1 1 1 d 1 1 1 d . . . 
+. . d d d 1 1 1 d d 1 1 1 d 1 . 
+. d d d 1 1 1 1 1 1 d d 1 1 1 . 
+d d d d d d d 1 1 1 1 1 1 1 1 . 
+. . . . d d d d d 1 1 1 d d 1 1 
+. . . . . . . . d d d d . . d d 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+. . . . . d d d d d . . . . . . 
+. . . . d d 1 d d 1 1 d . . . . 
+. . . . d 1 1 1 d 1 1 1 d . . . 
+. . d d d 1 1 1 d d 1 1 1 d 1 . 
+. d d d 1 1 1 1 1 1 d d 1 1 1 . 
+d d d d d d d 1 1 1 1 1 1 1 1 . 
+. . . . d d d d d 1 1 1 d d 1 1 
+. . . . . . . . d d d d . . d d 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+d d 1 1 1 1 1 d d 1 1 1 1 1 d d 
+1 d d d d d d d 1 1 1 1 d d d d 
+1 1 1 1 1 1 1 1 1 1 1 d d d d d 
+1 1 1 1 1 1 1 1 d d d d d d d d 
+d d 1 1 1 1 1 1 d d d 1 1 1 d 1 
+d d d 1 1 1 1 1 d d 1 1 1 1 1 1 
+d d d d d d 1 d d d d 1 1 1 1 1 
+1 d d d d d d d d d 1 1 1 d d d 
+1 1 1 d d d d d d d d d 1 1 1 1 
+1 1 1 1 1 d d 1 1 d d d 1 1 1 1 
+1 1 1 1 d d d d 1 1 1 d 1 1 1 1 
+1 1 1 1 1 d d d d d d d d d d 1 
+1 1 1 1 1 1 1 1 1 d d d d d d 1 
+d d d d 1 1 1 1 1 d d d d d d 1 
+1 1 1 1 d d d 1 1 1 1 1 d d d 1 
+1 1 1 1 1 1 d 1 1 1 1 1 1 1 1 1 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. d 1 1 d . . d d . . . . . . . 
+. . d 1 1 d d 1 1 d . . . . . . 
+. . . d 1 1 d d 1 1 1 d d d . . 
+. d d 1 1 1 1 d d d 1 1 1 1 d . 
+d d 1 1 1 d 1 1 1 d d 1 1 1 d . 
+d 1 d d d 1 1 1 1 1 1 1 1 d d d 
+. . . . 1 d d 1 1 1 1 1 1 1 1 . 
+. . . . . . . d 1 1 d d d 1 1 . 
+. . . . . . . . . . . . . 1 d d 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . d d 1 . . . . . . . . . 
+. . d 1 1 1 1 1 d d d . d d d d 
+. . . d d d 1 1 1 1 1 d 1 d 1 d 
+d 1 1 1 1 1 d 1 1 1 1 1 1 1 d . 
+d d d 1 1 1 1 d d 1 1 d d d . . 
+. . . d d 1 1 1 1 1 1 . . . . . 
+. . . . . d d d . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+d d d d d 1 1 1 1 d d d 1 1 1 1 
+1 1 d d 1 1 1 1 1 1 d d 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 d d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 d d 
+d 1 1 1 1 1 1 1 1 1 1 1 1 d d d 
+1 1 1 1 1 1 1 1 1 1 d d d d d d 
+1 1 1 1 1 1 1 1 1 1 d 1 1 1 1 d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+d d 1 1 1 1 1 1 1 1 1 1 1 1 d d 
+d d d d d 1 1 1 1 1 d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile9 = img`
+d d d d d d d d d d d d d d d d 
+d 1 1 d d d d d d d d d d d d d 
+1 1 1 1 d d d d d d d d d d d d 
+d d d d d d d d d 1 1 d d d d d 
+d d d d d d d d d d 1 1 d d d d 
+d d d d d d d d d d d d 1 1 1 1 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d 1 1 d d d d d d d d d 
+d d d d d d d 1 1 d d d d d d d 
+d d d d d d d d 1 d d d d d d d 
+d d 1 1 1 1 1 1 1 d d d d d d d 
+d 1 1 d d d d d d d d d d d d d 
+d d d d d d d d d d d d 1 1 1 1 
+d d d d d d d d d d d 1 1 d d d 
+d d d d d d d d 1 1 1 d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile10 = img`
+d d d d d 1 1 1 1 d d d 1 1 1 1 
+1 1 d d 1 1 1 1 1 1 d d d 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 d d d 
+d d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 d d d d d d 1 1 1 1 1 1 1 d d 
+1 1 1 1 1 1 d d d d 1 1 1 1 d d 
+d 1 1 1 1 1 1 1 1 1 d d d d d d 
+1 1 1 1 1 1 1 1 1 1 d d d d d d 
+d d 1 1 1 1 1 1 1 1 d 1 1 1 1 d 
+1 d d d d 1 1 1 1 1 1 1 1 1 1 d 
+1 1 1 1 d d d d d d d 1 1 1 1 d 
+1 1 1 1 1 1 1 1 1 1 d d 1 1 1 d 
+d 1 1 1 1 1 1 1 1 1 1 1 d d 1 d 
+d 1 1 1 1 1 1 1 1 1 1 1 1 d d d 
+d d 1 1 1 1 1 1 1 1 1 1 1 1 d d 
+d d d d d 1 1 1 1 1 d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile11 = img`
+d d d d d d 1 1 1 d d d d d d d 
+1 1 d d d 1 1 1 1 1 d d 1 1 1 1 
+1 1 1 1 1 d d d 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 d d 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+d d 1 1 1 1 1 1 1 1 1 1 1 1 d d 
+d d d 1 1 1 1 1 1 1 1 1 1 d d d 
+1 1 1 1 1 1 1 1 1 1 d d d d d d 
+1 1 1 1 1 1 d d d d d 1 1 1 1 d 
+1 1 1 d d d d d 1 1 1 1 1 1 1 d 
+1 1 1 1 1 d 1 1 1 1 1 1 1 1 1 d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d 
+d d d d 1 1 1 d 1 1 1 1 1 1 d d 
+d d d d d d d d d d 1 1 d d d d 
+`
+}
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100007070707070707070707070707070707070702070607060207060707060706070704070607070706070706070707060706020707070206070602020607020707070707060407060607020707070607070702070206070402060607070402070707020704040606020606060206020707060704020707060406070604070706070407020704060607020702060407070706070602060407070704040407040606070406040702040402020607040707070706070207070702070706070406070704040607060704020707040707060707020607040707070402060706040207070707060702070607070702070706070707060707040607070606070706060707`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11],
+            TileScale.Sixteen
+        ))
+scene.setBackgroundColor(9)
